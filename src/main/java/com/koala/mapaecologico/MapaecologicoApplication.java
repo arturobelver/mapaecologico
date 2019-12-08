f1ef1df48c27f098a14f45feed29c7ca0d1aa3ad
Arturo Belmonte <arturobelver@gmail.com>
2019-12-08 09:24:01 +0100

    Commit inicial

---
+++ src/main/java/com/koala/mapaecologico/MapaecologicoApplication.java
@@ -0,0 +1,13 @@
+package com.koala.mapaecologico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaecologicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaecologicoApplication.class, args);
+	}
+
+}
